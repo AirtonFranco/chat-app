58266885071580a8b7a0d41a1588885560d5aac9
Airton Silveira <airton.developer@gmail.com>
2020-09-23 02:33:00 -0300

    finalizando o front

--- backend/index.js
+++ backend/index.js
@@ -9,4 +9,12 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
+io.on('connection', (socket) => {
+    console.log("Temos uma nova conexao")
+
+    socket.on('disconnect', () => {
+        console.log("Usuario saiu");
+    })
+})
+
 server.listen(PORT, () => console.log(`Server has started. ${PORT}`));
